7d6b26a088dc26f8f388ffb5f3ddcba0ab5a9357
bravoporestefi <bravoporestefi@gmail.com>
2023-05-16 23:22:05 +0200

    Cambios

---
+++ archivo1.py
@@ -0,0 +1,7 @@
+#Funciones:
+
+def bit_and (n1, n2):
+        return print(f"El resultado es {bin(n1 & n2)}")
+    
+def bit_or (n1, n2):
+        return print(f"El resultado es {bin(n1 | n2)}")
---
+++ archivo2.py
@@ -0,0 +1,10 @@
+#Importaciones:
+
+from archivo1 import bit_and as andd
+from archivo1 import bit_or as orr
+
+num1= int(input("Dime un número: "))
+num2= int(input("Dime otro número (en la función invertir no se empleará): "))
+
+andd(num1, num2)
+orr(num1, num2)
